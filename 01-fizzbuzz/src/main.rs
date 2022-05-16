8262d2653f0b29544b0baf42e04f1c61166ad686
dosisod <39638017+dosisod@users.noreply.github.com>
2022-05-15 17:24:57 -0700

    Add fizzbuzz project:
    Since fizzbuzz is so simple, I implemented it in a few different ways. I prefer
    the match and iter solutions the best, since they seem the simplest/easiest to
    add on to.

---
+++ 01-fizzbuzz/src/main.rs
@@ -0,0 +1,75 @@
+fn basic_version() {
+    for i in 1..=100 {
+        if i % 15 == 0 {
+            println!("fizzbuzz");
+        } else if i % 3 == 0 {
+            println!("fizz");
+        } else if i % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
+
+fn alternate_version() {
+    for i in 1..=100 {
+        let is_multiple_of_3 = i % 3 == 0;
+        let is_multiple_of_5 = i % 5 == 0;
+
+        if is_multiple_of_3 {
+            print!("fizz");
+        }
+        if is_multiple_of_5 {
+            print!("buzz");
+        }
+
+        if is_multiple_of_3 || is_multiple_of_5 {
+            println!("");
+        } else {
+            println!("{}", i);
+        }
+    }
+}
+
+fn match_version() {
+    for i in 1..=100 {
+        match i {
+            i if i % 15 == 0 => println!("fizzbuzz"),
+            i if i % 3 == 0 => println!("fizz"),
+            i if i % 5 == 0 => println!("buzz"),
+            _ => println!("{}", i),
+        };
+    }
+}
+
+fn iter_version() {
+    (1..=100)
+        .map(|i| {
+            if i % 15 == 0 {
+                "fizzbuzz".to_string()
+            } else if i % 3 == 0 {
+                "fizz".to_string()
+            } else if i % 5 == 0 {
+                "buzz".to_string()
+            } else {
+                format!("{}", i)
+            }
+        })
+        .for_each(|x| println!("{}", x.to_string()));
+}
+
+fn main() {
+    let line_break = || println!("-----------");
+
+    basic_version();
+    line_break();
+
+    alternate_version();
+    line_break();
+
+    match_version();
+    line_break();
+
+    iter_version();
+}
